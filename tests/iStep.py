1bb2927ff31fe04439f351641e6ae1f4ed456783
Blanchot <Blanchot@users.noreply.github.com>
2018-04-12 08:41:06 +0200

    Test two motors

--- tests/iStep.py
+++ tests/iStep.py
@@ -1,10 +1,56 @@
 #iStep.py
 #Code from here: http://ingeniapp.com/en/stepper-motor-control-with-raspberry-pi/
-#Test single motor selection
+#Test two motors
 import time
 import sys
 import RPi.GPIO as GPIO
 
+def m1step_8 (p):
+  if p==0:
+    GPIO.output(23,0)
+    GPIO.output(24,0)
+    GPIO.output(25,0)
+    GPIO.output(4,0)
+  if p==1:
+    GPIO.output(23,1)
+    GPIO.output(24,0)
+    GPIO.output(25,0)
+    GPIO.output(4,0)
+  if p==2:
+    GPIO.output(23,1)
+    GPIO.output(24,1)
+    GPIO.output(25,0)
+    GPIO.output(4,0)
+  if p==3:
+    GPIO.output(23,0)
+    GPIO.output(24,1)
+    GPIO.output(25,0)
+    GPIO.output(4,0)
+  if p==4:
+    GPIO.output(23,0)
+    GPIO.output(24,1)
+    GPIO.output(25,1)
+    GPIO.output(4,0)
+  if p==5:
+    GPIO.output(23,0)
+    GPIO.output(24,0)
+    GPIO.output(25,1)
+    GPIO.output(4,0)
+  if p==6:
+    GPIO.output(23,0)
+    GPIO.output(24,0)
+    GPIO.output(25,1)
+    GPIO.output(4,1)
+  if p==7:
+    GPIO.output(23,0)
+    GPIO.output(24,0)
+    GPIO.output(25,0)
+    GPIO.output(4,1)
+  if p==8:
+    GPIO.output(23,1)
+    GPIO.output(24,0)
+    GPIO.output(25,0)
+    GPIO.output(4,1)
 
 def m2step_8 (p):
   if p==0:
@@ -12,19 +58,16 @@ def m2step_8 (p):
     GPIO.output(6,0)
     GPIO.output(12,0)
     GPIO.output(13,0)
-
   if p==1:
     GPIO.output(5,1)
     GPIO.output(6,0)
     GPIO.output(12,0)
     GPIO.output(13,0)
-
   if p==2:
     GPIO.output(5,1)
     GPIO.output(6,1)
     GPIO.output(12,0)
     GPIO.output(13,0)
-
   if p==3:
     GPIO.output(5,0)
     GPIO.output(6,1)
@@ -56,10 +99,28 @@ def m2step_8 (p):
     GPIO.output(12,0)
     GPIO.output(13,1)
 
-def m2steps_8(value):
+def m1steps_8(value):
   print(value)
   global pas
-  
+  if(value<0):
+    for i in range (0,abs(value)):
+      m1step_8(pas)
+      time.sleep(0.005)
+      pas+=1
+      if(pas>=9):
+        pas=1;
+  else:
+    for i in range (0,abs(value)):
+      m1step_8(pas)
+      time.sleep(0.005)
+      if(pas==1):
+        pas=9;
+      pas-=1
+  m1step_8(0)
+
+def m2steps_8(value):
+  print(value)
+  global pas 
   if(value<0):
     for i in range (0,abs(value)):
       m2step_8(pas)
