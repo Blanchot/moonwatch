5331b16070fb23c71fa5711ed92f7f38ca9a3cda
Blanchot <Blanchot@users.noreply.github.com>
2018-04-12 08:30:08 +0200

    Test single motor selection

--- tests/iStep.py
+++ tests/iStep.py
@@ -1,12 +1,12 @@
 #iStep.py
 #Code from here: http://ingeniapp.com/en/stepper-motor-control-with-raspberry-pi/
-#Removed 4 step code and added preliminary motor selection
+#Test single motor selection
 import time
 import sys
 import RPi.GPIO as GPIO
 
 
-def step_8 (p):
+def m2step_8 (p):
   if p==0:
     GPIO.output(5,0)
     GPIO.output(6,0)
@@ -56,35 +56,44 @@ def step_8 (p):
     GPIO.output(12,0)
     GPIO.output(13,1)
 
-def steps_8(value):
+def m2steps_8(value):
   print(value)
   global pas
+  
   if(value<0):
     for i in range (0,abs(value)):
-      step_8(pas)
+      m2step_8(pas)
       time.sleep(0.005)
       pas+=1
       if(pas>=9):
         pas=1;
   else:
     for i in range (0,abs(value)):
-      step_8(pas)
+      m2step_8(pas)
       time.sleep(0.005)
       if(pas==1):
         pas=9;
       pas-=1
-  step_8(0)
+  m2step_8(0)
 
 if __name__ == "__main__":
   
   GPIO.setmode(GPIO.BCM)
   GPIO.setwarnings(False)
+  #motor 0 (m0)
+  #motor 1 (m1)
+  GPIO.setup(23, GPIO.OUT)
+  GPIO.setup(24, GPIO.OUT)
+  GPIO.setup(25, GPIO.OUT)
+  GPIO.setup(4, GPIO.OUT)
+  #motor 2 (m2)
   GPIO.setup(5, GPIO.OUT)
   GPIO.setup(6, GPIO.OUT)
   GPIO.setup(12, GPIO.OUT)
   GPIO.setup(13, GPIO.OUT)
+  #motor 3 (m3)
 
-  step_8(0) #changed in original code from step_4(0)
+  #step_8(0) #changed in original code from step_4(0)
   pas=1
   print(len(sys.argv))
   if(len(sys.argv)<2):
@@ -102,7 +111,7 @@ if __name__ == "__main__":
       #steps_8(st)
     elif(len(sys.argv)==3 and sys.argv[2]=="2"):
       print("m2: left azimuth")
-      steps_8(st)
+      m2steps_8(st)
     elif(len(sys.argv)==3 and sys.argv[2]=="3"):
       print("m3: left altitude")
       #steps_8(st)
