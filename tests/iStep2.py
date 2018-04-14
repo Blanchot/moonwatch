fa646ac1b04d1c5b0220c889c32fb6286c6ba13b
Blanchot <Blanchot@users.noreply.github.com>
2018-04-14 15:04:17 +0200

    Bug fix

--- tests/iStep2.py
+++ tests/iStep2.py
@@ -258,7 +258,7 @@ def m1steps_8(value):
     for i in range (0,abs(value)):
       m1step_8(pas)
       time.sleep(0.01)
-      m1 += #add 1 to counter
+      m1 += 1 #add 1 to counter
       if(pas==1):
         pas=9;
       pas-=1
