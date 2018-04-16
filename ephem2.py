130c21c57df44381575fa52f9a679eda32958519
Blanchot <Blanchot@users.noreply.github.com>
2018-04-16 09:34:30 +0200

    Added sunAlt calculations

--- ephem2.py
+++ ephem2.py
@@ -10,21 +10,35 @@ import time
 stepCircle = 4100 #number of steps to turn 360
 steps_1_deg = stepCircle/360
 
+old_m0_stepCount = 0 #old SunAlt
+cur_m0_stepCount = 0 #current SunAlt
 old_m1_stepCount = 0 #old sunAz
 cur_m1_stepCount = 0 #current sunAz
+old_m2_stepCount = 0 #old moonAz
+cur_m2_stepCount = 0 #current moonAz
+old_m3_stepCount = 0 #old moonAlt
+cur_m3_stepCount = 0 #current moonAlt
 
 def init():
   global cur_m1_stepCount
   cur_m1_stepCount = 0 #number of steps clockwise
 
+def m0_update(): #sunAlt
+  global old_m0_stepCount
+  global cur_m0_stepCount
+  cur_m0_stepCount = round(moonAz * steps_1_deg)
+  m0_takeSteps = cur_m0_stepCount - old_m0_stepCount
+  print('m0 stepCount (old: {}, cur: {}, dif: {})'.format(old_m0_stepCount,cur_m0_stepCount,m0_takeSteps))
+  old_m0_stepCount = cur_m0_stepCount
+
 def m1_update(): #sunAz
   global old_m1_stepCount
-  print('old_m1_stepCount: {}'.format(old_m1_stepCount))
+  #print('old_m1_stepCount: {}'.format(old_m1_stepCount))
   global cur_m1_stepCount
   cur_m1_stepCount = round(sunAz * steps_1_deg)
-  print('cur_m1_stepCount: {}'.format(cur_m1_stepCount))
+  #print('cur_m1_stepCount: {}'.format(cur_m1_stepCount))
   m1_takeSteps = cur_m1_stepCount - old_m1_stepCount
-  print('m1_takeSteps: {}'.format(m1_takeSteps))
+  #print('m1_takeSteps: {}'.format(m1_takeSteps))
   print('m1 stepCount (old: {}, cur: {}, dif: {})'.format(old_m1_stepCount,cur_m1_stepCount,m1_takeSteps))
   old_m1_stepCount = cur_m1_stepCount
 
@@ -53,10 +67,11 @@ while True:
   moonAlt = int(math.degrees(moon.alt))
   
   print(time.ctime())
-  print('Sun   Azimuth:',sunAz)
-  print('Sun  Altitude:',sunAlt)
-  print('Moon  Azimuth:',moonAz)
-  print('Moon Altitude:',moonAlt)
+  print('Sun  Altitude (m0):',sunAlt)
+  print('Sun   Azimuth (m1):',sunAz)
+  print('Moon  Azimuth (m2):',moonAz)
+  print('Moon Altitude (m3):',moonAlt)
+  m0_update()
   m1_update()
   print()
   time.sleep(300)
